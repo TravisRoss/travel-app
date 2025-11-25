98a8c94a14a8498f5a3079912eac44ce0326b50a
Travis Ross <travis.ross@gira.de>
2025-11-25 22:02:00 +0100

    feat: :sparkles: implement ability to toggle/"pack" items

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <>
       <Logo />
       <Form onAddItems={handleAddItem} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
       <Stats />
     </>
   );
@@ -70,20 +78,20 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       {items.map((item) => (
-        <Item item={item} onDeleteItem={onDeleteItem} />
+        <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
       ))}
     </div>
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
-      <input type="checkbox" />
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
